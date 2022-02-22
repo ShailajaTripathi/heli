81f5d67a35dac791a1a3d5b6c7d423608336f807
ShailajaTripathi <shailaja24305@gmail.com>
2022-02-22 16:09:24 +0530

    Home

---
+++ myheli/src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './about.css'
+
+function about() {
+  return (
+    <div className="title">
+        <h1>LIFTING THE FUTURE</h1>
+        
+
+    </div>
+  )
+}
+
+export default about
---
+++ myheli/src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+  return (
+    <div className="container">
+        <div className="top-bg"></div>
+        <div className="home-section">
+          <h1 className="head-line">NEW FORKLIFTS FOR SALE</h1>
+          <p className="about">Lifting the Future</p>
+          <div className="home-button">
+              <button  className="home-button-item">Find out more</button>
+              </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
